e91495b1b0bc9ab319136e592aeba92caeff6816
carcl <11902846+carcl@users.noreply.github.com>
2025-02-28 14:41:34 -0600

    file rename import fix

--- applications/dots_and_boxes/NNmodels/model.py
+++ applications/dots_and_boxes/NNmodels/model.py
@@ -1,7 +1,7 @@
 import torch
 import torch.nn.functional as F
 from typing import Dict, Tuple
-from core.model import ModelInterface
+from core.model_interface import ModelInterface
 from core.implementations.AlphaZero import AlphaZeroTarget
 from applications.dots_and_boxes.game_state import *
 
