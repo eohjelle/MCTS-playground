fa34ddbce497ebb56988c84c7556f88d860eda8c
Eivind Otto Hjelle <eohjelle@gmail.com>
2026-01-18 21:02:38 -0800

    untrack some files

--- experiments/distributional_alphazero/models/__init__.py
+++
@@ -1,9 +0,0 @@
-from .resmlp import ResMLP, ResMLPInitParams
-from .resnet import ResNet, ResNetInitParams
-
-__all__ = [
-    "ResMLP",
-    "ResMLPInitParams",
-    "ResNet",
-    "ResNetInitParams",
-] 
